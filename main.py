2b8c563c7b9bd3bc08103708877c73a0b21b3ef0
John Knoll <jpknoll@ucdavis.edu>
2017-07-29 12:01:59 -0700

    image graphing

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os
 import numpy as np
 import tensorflow as tf
 from flask import Flask, jsonify, render_template, request
+from werkzeug.utils import secure_filename
 
 from mnist import model
-
 
 x = tf.placeholder("float", [None, 784])
 sess = tf.Session()
@@ -13,7 +14,6 @@ with tf.variable_scope("regression"):
     y1, variables = model.regression(x)
 saver = tf.train.Saver(variables)
 saver.restore(sess, "./mnist/data/regression.ckpt")
-
 
 with tf.variable_scope("convolutional"):
     keep_prob = tf.placeholder("float")
@@ -30,8 +30,78 @@ def convolutional(input):
     return sess.run(y2, feed_dict={x: input, keep_prob: 1.0}).flatten().tolist()
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def load_graph(model_file):
+    graph = tf.Graph()
+    graph_def = tf.GraphDef()
+
+    with open(model_file, "rb") as f:
+        graph_def.ParseFromString(f.read())
+    with graph.as_default():
+        tf.import_graph_def(graph_def)
+
+    return graph
+
+
+def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
+                                input_mean=0, input_std=255):
+    input_name = "file_reader"
+    output_name = "normalized"
+    file_reader = tf.read_file(file_name, input_name)
+    if file_name.endswith(".png"):
+        image_reader = tf.image.decode_png(file_reader, channels=3,
+                                           name='png_reader')
+    elif file_name.endswith(".gif"):
+        image_reader = tf.squeeze(tf.image.decode_gif(file_reader,
+                                                      name='gif_reader'))
+    elif file_name.endswith(".bmp"):
+        image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
+    else:
+        image_reader = tf.image.decode_jpeg(file_reader, channels=3,
+                                            name='jpeg_reader')
+    float_caster = tf.cast(image_reader, tf.float32)
+    dims_expander = tf.expand_dims(float_caster, 0);
+    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+    normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+    sess = tf.Session()
+    result = sess.run(normalized)
+
+    return result
+
+
+def load_labels(label_file):
+    label = []
+    proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+    for l in proto_as_ascii_lines:
+        label.append(l.rstrip())
+    return label
+
+
 # webapp
 app = Flask(__name__)
+
+UPLOAD_FOLDER = './upload'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+graph = load_graph("./models/greenergraph.pb")
+labels = load_labels("./models/greenerlabels.txt")
+
+input_layer = "Mul"
+output_layer = "final_result"
+
+input_name = "import/" + input_layer
+output_name = "import/" + output_layer
+input_operation = graph.get_operation_by_name(input_name);
+output_operation = graph.get_operation_by_name(output_name);
+
 
 
 @app.route('/api/mnist', methods=['POST'])
@@ -45,7 +115,25 @@ def mnist():
 @app.route('/api/image', methods=['POST'])
 def image():
     file = request.files['file']
-    bin_file = StringIO.StringIO(file.read())
+    filename = secure_filename(file.filename)
+    filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filename)
+
+    t = read_tensor_from_image_file(filename)
+    with tf.Session(graph=graph) as sess:
+        results = sess.run(output_operation.outputs[0],
+                           {input_operation.outputs[0]: t})
+        #results = np.squeeze(results)
+
+        results = results.flatten().tolist()
+        #top_k = results.argsort()[-5:][::-1]
+
+        data = []
+        for i in range(len(results)):
+            data.append({'label': labels[i], 'value': results[i]})
+
+        return jsonify(data)
+
 
 @app.route('/')
 def main():
